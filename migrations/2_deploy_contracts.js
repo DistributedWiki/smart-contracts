69e7c713292f9879281d9cfa1dbe10f2822c11fd
chorig9 <chorig9@gmail.com>
2018-05-01 16:48:08 +0200

    Add TopLevel contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Article = artifacts.require("./Article.sol");
+var TopLevel = artifacts.require("./TopLevel.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(Article, "0x0000000000000000000000000000000000000000000000000000000000000002".valueOf());
+    deployer.deploy(TopLevel);
 };
